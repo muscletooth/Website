f7c29c900deb2bc7b0600221f608a9f4c9be38ce
Ubuntu <ubuntu@ip-172-31-25-65.us-east-2.compute.internal>
2020-06-24 19:16:28 +0000

    latest

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tim Rhoades | Home' });
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Tim Rhoades | About Me' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Tim Rhoades | Contact Us' });
+});
+
+
 
 module.exports = router;
